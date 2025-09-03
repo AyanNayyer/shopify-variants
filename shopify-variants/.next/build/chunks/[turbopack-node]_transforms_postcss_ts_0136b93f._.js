e08da557a8eeb569018c18879169aeb10628048b
Ayan Nayyer <ayannayyer@gmail.com>
2025-09-03 17:16:46 +0530

    vercel deployment

---
+++ shopify-variants/.next/build/chunks/[turbopack-node]_transforms_postcss_ts_0136b93f._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Documents/GitHub/shopify-variants/shopify-variants/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "build/chunks/5e672_666096f9._.js",
+  "build/chunks/[root-of-the-server]__abd90ea6._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Documents/GitHub/shopify-variants/shopify-variants/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ shopify-variants/.next/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./build/chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("build/chunks/[turbopack-node]_transforms_postcss_ts_0136b93f._.js")
+R.c("build/chunks/[root-of-the-server]__c6aaeda0._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/GitHub/shopify-variants/shopify-variants/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/GitHub/shopify-variants/shopify-variants/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
